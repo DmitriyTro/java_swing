8c4cf21cc2435cb7003b807ff529cbc3682400a8
Dmitrii <probcf@gmail.com>
2021-09-07 00:30:52 +0300

    add buttons

--- src/com/dmitriitrofimov/Main.java
+++ src/com/dmitriitrofimov/Main.java
@@ -1,21 +1,28 @@
 package com.dmitriitrofimov;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Main {
 	public static void main(String[] args) {
-		JFrame frame = new JFrame();
+		JFrame frame = new JFrame("this is title form...");
 
 		frame.setSize(600, 400);
-		frame.setTitle("this is title form...");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setLocationRelativeTo(null);
-		frame.setResizable(true);
+		frame.setLayout(new GridBagLayout());
 
+		JButton button = new JButton("button");
+		JButton button2 = new JButton("button 2");
+		button.setCursor(new Cursor(Cursor.W_RESIZE_CURSOR));
+
+		button.setBackground(Color.YELLOW);
+		button.setForeground(Color.RED);
+		Color color = button.getBackground();
+		button2.setBackground(color);
+
+		frame.add(button);
+		frame.add(button2);
 		frame.setVisible(true);
-
-//		frame.setState(JFrame.ICONIFIED);
-//		frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
-//		frame.setExtendedState(JFrame.NORMAL);
 	}
 }
