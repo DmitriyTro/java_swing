395f65d1b0e43250e36f1230ba18b767d498851a
Dmitrii <probcf@gmail.com>
2021-09-11 01:07:10 +0300

    JMenu part_2

---
+++ src/com/dmitriitrofimov/JMenu_PartTwoTest.java
@@ -0,0 +1,39 @@
+package com.dmitriitrofimov;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class JMenu_PartTwoTest {
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("this is title form...");
+
+		frame.setSize(600, 400);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLocationRelativeTo(null);
+		frame.setLayout(new GridBagLayout());
+
+		JMenuBar jMenuBar = new JMenuBar();
+		JMenu menu1 = new JMenu("menu_1");
+		JMenu menu2 = new JMenu("menu_2");
+		jMenuBar.add(menu1);
+		jMenuBar.add(menu2);
+
+		JMenu itemMenu1 = new JMenu("item_menu_1");
+		JMenu itemMenu2 = new JMenu("item_menu_2");
+		JMenu itemMenu3 = new JMenu("item_menu_3");
+		menu1.add(itemMenu1);
+		menu1.add(itemMenu2);
+		menu1.add(itemMenu3);
+
+		JMenuItem subMenuItem1 = new JMenuItem("sub_menu_item_1");
+		JMenuItem subMenuItem2 = new JMenuItem("sub_menu_item_2");
+		JMenuItem subMenuItem3 = new JMenuItem("sub_menu_item_3");
+		itemMenu1.add(subMenuItem1);
+		itemMenu1.add(subMenuItem2);
+		itemMenu1.add(subMenuItem3);
+
+
+		frame.setJMenuBar(jMenuBar);
+		frame.setVisible(true);
+	}
+}
