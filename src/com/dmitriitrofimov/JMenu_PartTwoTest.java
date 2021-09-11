046448becb87667edcac759101faddbf1e2d1683
Dmitrii <probcf@gmail.com>
2021-09-11 14:08:23 +0300

    JRadioButtonMenuItem and JCheckBoxMenuItem

--- src/com/dmitriitrofimov/JMenu_PartTwoTest.java
+++ src/com/dmitriitrofimov/JMenu_PartTwoTest.java
@@ -25,14 +25,25 @@ public class JMenu_PartTwoTest {
 		menu1.add(itemMenu2);
 		menu1.add(itemMenu3);
 
+		itemMenu2.setIcon(new ImageIcon("plus.png"));
+
+		JRadioButtonMenuItem r1 = new JRadioButtonMenuItem("radio_button_1");
+		JRadioButtonMenuItem r2 = new JRadioButtonMenuItem("radio_button_2");
+		JCheckBoxMenuItem r3 = new JCheckBoxMenuItem("check_box");
+
+		ButtonGroup buttonGroup = new ButtonGroup();
+		buttonGroup.add(r1);
+		buttonGroup.add(r2);
+		itemMenu1.add(r1);
+		itemMenu1.add(r2);
+		itemMenu1.add(r3);
+
 		JMenuItem subMenuItem1 = new JMenuItem("sub_menu_item_1");
 		JMenuItem subMenuItem2 = new JMenuItem("sub_menu_item_2");
 		JMenuItem subMenuItem3 = new JMenuItem("sub_menu_item_3");
 		itemMenu1.add(subMenuItem1);
 		itemMenu1.add(subMenuItem2);
 		itemMenu1.add(subMenuItem3);
-
-		itemMenu2.setIcon(new ImageIcon("plus.png"));
 
 		frame.setJMenuBar(jMenuBar);
 		frame.setVisible(true);
