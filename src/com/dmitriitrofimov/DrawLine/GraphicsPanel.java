ab2a0988cd0b77704e0ffcc849b7b071e80d2085
Dmitrii <probcf@gmail.com>
2021-09-11 17:13:27 +0300

    draw rectangle and circle on the form

--- src/com/dmitriitrofimov/DrawLine/GraphicsPanel.java
+++ src/com/dmitriitrofimov/DrawLine/GraphicsPanel.java
@@ -7,10 +7,16 @@ public class GraphicsPanel extends JPanel {
 
 	private Line line;
 	private Line line2;
+	private Rectangle rectangle;
+	private Circle circle;
 
 	public GraphicsPanel() {
 		line = new Line(25, 150, 78, 2);
 		line2 = new Line(210, 55, 108, 32);
+
+		rectangle = new Rectangle(170, 200, 75, 100);
+
+		circle = new Circle(100, 250, 75, 75);
 	}
 
 	@Override
@@ -19,5 +25,7 @@ public class GraphicsPanel extends JPanel {
 
 		g2.draw(line);
 		g2.draw(line2);
+		g2.draw(rectangle);
+		g2.draw(circle);
 	}
 }
